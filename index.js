d2803df93a647243316e3320ad3228a9e6ca66af
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-08-28 18:02:42 -0300

    Adding css and style

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let net;
 const webcamElement = document.getElementById('webcam');
+const multAddAction = document.getElementById('multAddAction');
+const labelA = document.getElementById('label-a');
+const labelB = document.getElementById('label-b');
+const labelC = document.getElementById('label-c');
 const classifier = knnClassifier.create();
 
 async function setupWebcam() {
@@ -33,13 +37,26 @@ async function app() {
     // Reads an image from the webcam and associates it with a specific class
     // index.
     const addExample = classId => {
-        // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-        // to the KNN classifier.
-        const activation = net.infer(webcamElement, 'conv_preds');
-
-        // Pass the intermediate activation to the classifier.
-        classifier.addExample(activation, classId);
+        var numbAddActions = multAddAction.value;
+        console.log(numbAddActions);
+        var count = 0;
+        do {
+            // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+            // to the KNN classifier.
+            resizeVideo(224);
+            const activation = net.infer(webcamElement, 'conv_preds');
+            // Pass the intermediate activation to the classifier.
+            classifier.addExample(activation, classId);
+            count++;
+            console.log("Add more one element");
+        } while (count < numbAddActions);
+        resizeVideo(500);
     };
+
+    const resizeVideo = resizePixel => {
+        webcamElement.style.width = resizePixel;
+        webcamElement.style.height = resizePixel;
+    }
 
     // When clicking a button, add an example for that class.
     document.getElementById('class-a').addEventListener('click', () => addExample(0));
@@ -53,10 +70,10 @@ async function app() {
             // Get the most likely class and confidences from the classifier module.
             const result = await classifier.predictClass(activation);
 
-            const classes = ['A', 'B', 'C'];
+            const classes = [labelA.value, labelB.value, labelC.value];
             document.getElementById('console').innerText = `
-          prediction: ${classes[result.classIndex]}\n
-          probability: ${result.confidences[result.classIndex]}
+          Prediction: ${classes[result.classIndex]}
+          Probability: ${result.confidences[result.classIndex]}
         `;
         }
 
